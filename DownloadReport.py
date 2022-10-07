cb2e556faae45e82e45b38e1e86f348586a34f87
ekakuman <ephraimkn1976@gmail.com>
2022-10-07 10:38:18 -0700

    Add files via upload

---
+++ DownloadReport.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+""" Python Script to Search for Users from Calling Engagement Report Org in Control Hub
+    
+This script is designed to delete users from a Control Hub organization based on an INPUT CSV file with user emails.
+The script is designed to be executed by users with "full admin" role in the org.
+Output file called Errors.csv is generated at the end in the same directory as the input CSV file 
+(Errors.csv will be empty if the script runs successfully without errors)
+Tested with Python version 3.6
+The script is limited to only allow 100 users per input file.
+If you need to delete more than 100 users then you need to split users into multiple CSV input files or modify the script.
+
+The CSV file should be created using the format from the CSV export from Control Hub.
+- Keep the rows of users that should be deleted and REMOVE USERS who should NOT be deleted
+- Do not delete the header line, or the first user to delete will be skipped
+- Very little validation is done of the input file.  Deleting fields can break this script.
+- (The fourth field of each row is the email of the user that will be deleted)
+- IMPORTANT:  THE FILE SHOULD ONLY HAVE ENTRIES OF USERS THAT SHOULD BE DELETED
+
+"""
+
+__author__ = "Matt Klawiter"
+__date__ = "2020/4/29"
+
+__modification_by_author__ = "Ephraim Kakumani"
+__date__ = "2022/9/19"
+
+#############  Imports  #############
+import requests
+import json
+
+#############  Definitions  #############
+csvFilePath = ''                                                        # Update this value to skip entering command line input
+accessToken = ''                                                        # Update this value to skip entering command line input
+templateId = 116;
+errorMessage = ''
+reportURL = 'https://webexapis.com/v1/reports'         # Webex CH List People API URL
+getMyDetailsURL = 'https://webexapis.com/v1/people/me' # Webex CH Get My Details API URL
+
+#############   User Input and Validation  #############
+print('This script requires two inputs:')
+print('    1. An access token used to authorize the API calls\n       (You can get yours from https://developer.webex.com/docs/api/getting-started)')
+
+validationSuccess = 0
+
+# Loop to allow the user to input an access token until successful.
+while (validationSuccess == 0):
+    if not accessToken :
+        accessToken = input('Please enter your access token:  ')
+
+    # Get People API Call to validate access token.
+    validationResponse = requests.post(getMyDetailsURL,
+                headers={'Authorization': 'Bearer ' + accessToken})
+    if validationResponse.status_code == 401:
+        # This means the access token was invalid.
+        print('Access Token was invalid.  Please check your access token was entered correctly and hasn\'t expired and try again below.\n')
+        accessToken = ''
+    else:
+        reportFile = open('reportid.json')
+        data = json.load(reportFile)
+        reportID = data['items']['Id']
+        reportFile.close()
+
+        reportResponse = requests.get(reportURL + '?reportId=' + reportID,
+                                      headers={'Authorization': 'Bearer ' + accessToken,
+                                                'Content-Type': 'application/json'})
+        # Pausing script for a 429 Error
+        while reportResponse.status_code == 429:
+            print('Webex returned a 429 response (too many API calls at once).  Pausing script for 30 seconds...')
+            time.sleep(30)
+            reportResponse = requests.get(reportURL + '?reportId=' + reportID,
+                                      headers={'Authorization': 'Bearer ' + accessToken,
+                                                'Content-Type': 'application/json'})
+        if reportResponse.status_code != 200:
+            # This means something went wrong.
+            print('Error: Report Creation API call error', str(reportResponse.status_code))
+            errorMessage = reportResponse.json()['message']
+            with open('Errors.csv','a') as csvfile:
+                csvfile.write(str(reportResponse.status_code) + ',' + errorMessage + '\n')
+        else:
+            statusDownload = reportResponse.json()['items'][0]['status']
+            urlDownload = reportResponse.json()['items'][0]['downloadURL']
+
+            # Save the report file to csv if the download status is successful
+            if statusDownload != 'done':
+                print('The report generation is in progress. Please try again after sometime', '\n')
+            else:
+                print('Report download URl: \n', urlDownload, '\n')
+                reportDownload = requests.get(urlDownload,
+                                        headers={'Authorization': 'Bearer ' + accessToken})
+                urlContent = reportDownload.content
+                csvFile = open('report.csv', 'wb')
+                csvFile.write(urlContent)
+                csvFile.close()
+                print('The report downloaded successfully. Please check the local folder for file report.csv \n')
+        validationSuccess = 1
+print('Please check for errors in the file Errors.csv.\n')
+#############   End User Input  #############
---
+++ SearchReport.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+""" Python Script to Search for Users from Calling Engagement Report Org in Control Hub
+    
+This script is designed to delete users from a Control Hub organization based on an INPUT CSV file with user emails.
+The script is designed to be executed by users with "full admin" role in the org.
+Output file called Errors.csv is generated at the end in the same directory as the input CSV file 
+(Errors.csv will be empty if the script runs successfully without errors)
+Tested with Python version 3.6
+The script is limited to only allow 100 users per input file.
+If you need to delete more than 100 users then you need to split users into multiple CSV input files or modify the script.
+
+The CSV file should be created using the format from the CSV export from Control Hub.
+- Keep the rows of users that should be deleted and REMOVE USERS who should NOT be deleted
+- Do not delete the header line, or the first user to delete will be skipped
+- Very little validation is done of the input file.  Deleting fields can break this script.
+- (The fourth field of each row is the email of the user that will be deleted)
+- IMPORTANT:  THE FILE SHOULD ONLY HAVE ENTRIES OF USERS THAT SHOULD BE DELETED
+
+"""
+
+__author__ = "Matt Klawiter"
+__date__ = "2020/4/29"
+
+__modification_by_author__ = "Ephraim Kakumani"
+__date__ = "2022/9/19"
+
+#############  Imports  #############
+import requests
+import json
+import os
+import csv
+import urllib
+import time
+import pandas as pd
+import time
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.expand_frame_repr', False)
+pd.set_option('max_colwidth', None)
+pd.set_option("display.max_rows", None)
+
+#############  Definitions  #############
+csvFilePath = ''                                                        # Update this value to skip entering command line input
+
+def display_():    
+    from IPython.core.display import display 
+    display(df_group_calls.first()) #df must be defined up there
+
+#############   User Input and Validation  #############
+print('This script requires two inputs:')
+print('    1. The full file path on your device for an input CSV file\n (eg: C:\Scripts\exported_file.csv on Windows or ~/Scripts/exported_file.csv on Mac)\n')
+
+validationSuccess = 0
+# Loop to allow the user to input a file path and file name until successful.
+while (validationSuccess == 0):
+    if not csvFilePath :
+        csvFilePath = input('Please enter the full file path of the CSV file you wish to use:  ')
+    # Validate the Input CSV file exists.
+    csvFilePath = os.path.expanduser(csvFilePath)
+    if( not os.path.isfile(csvFilePath) ):
+        print('No Input CSV file found on your device at: ' + csvFilePath)
+        print('Please check the file path you entered above and try again.\n')
+        csvFilePath = ''
+    else:
+        validationSuccess = 1
+print('Input CSV file found at: ', csvFilePath, '\n')
+errorFilePath = os.path.join(os.path.dirname(csvFilePath),'Errors.csv')
+
+print('Loading CSV file. Please wait....', '\n')
+
+startTime = time.time()
+df = pd.read_csv(csvFilePath)
+endTime = time.time()
+
+print("Total number of records in CSV file :", len(df), '\n')
+print("The time to load CSV file is :", round(endTime-startTime), "s", '\n')
+#print("The time to load CSV file is :", round(endTime-startTime) * 10**3, "ms", '\n')
+email_ids = list(map(str, input("Enter email/s: ").split()))
+
+print('\n',"List of User Email Ids: ", email_ids, '\n')
+#############   End User Input  and validation #############
+
+#############   Search users and Display results  #############
+
+df_email = pd.DataFrame()
+
+for i in range(len(email_ids)):
+    df_email_init = df.loc[(df['Email'] == email_ids[i])]
+    df_email = pd.concat([df_email , df_email_init])
+
+df_email_array = df_email['Call ID'].values
+df_email_calls = df.loc[df['Call ID'].isin(df_email_array)]
+
+df_group_calls = df_email_calls.groupby(['Call ID','Email'])
+
+df_group = pd.DataFrame(df_email_calls.groupby(['Call ID','Email','Name']).size(),columns=['Count'])
+#print('Total Number of Call Records', len(df_group_calls['Call ID'].unique()), '\n')
+
+print(df_group_calls.first())
+#df_group_calls.to_excel('SearchOutput.xls', index=False)
+df_group.to_csv('SearchOutput.csv',index=True)
+#display_()
+
+#############   End Search users and Display results  #############
+
+"""
+Copyright 2022 <Cisco Systems inc>
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
+to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
